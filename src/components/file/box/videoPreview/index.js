5b1ec21638b6fdd9e6f1dbfbd0806e5f0bd98f20
zsxfa <1844577897@qq.com>
2022-04-08 22:55:07 +0800

    4.8修改

--- src/components/file/box/videoPreview/index.js
+++ src/components/file/box/videoPreview/index.js
@@ -11,32 +11,41 @@ let videoPreviewInstance = null
  * @param {object} defaultIndex 当前查看的视频索引
  */
 const initInstanceVideoPreview = (videoList, defaultIndex) => {
-	videoPreviewInstance = new VideoPreviewConstructor({
-		el: document.createElement('div'),
-		data() {
-			return {
-				videoList,
-				defaultIndex
-			}
-		}
-	})
+  videoPreviewInstance = new VideoPreviewConstructor({
+    el: document.createElement('div'),
+    data() {
+      return {
+        videoList,
+        defaultIndex,
+      }
+    },
+  })
 }
 /**
  * 视频预览 Promise 函数
  * @returns {Promise} 抛出确认和取消回调函数
  */
 const showVideoPreviewBox = (obj) => {
-	let { videoList, defaultIndex } = obj
-	return new Promise((reslove) => {
-		initInstanceVideoPreview(videoList, defaultIndex)
-		videoPreviewInstance.callback = (res) => {
-			reslove(res)
-		}
-		document.body.appendChild(videoPreviewInstance.$el) //  挂载 DOM
-		Vue.nextTick(() => {
-			videoPreviewInstance.visible = true //  打开视频预览遮罩层
-		})
-	})
+  // 非首次调用服务时，在 DOM 中移除上个实例
+  if (videoPreviewInstance !== null) {
+    document.body.removeChild(videoPreviewInstance.$el)
+  }
+  let { videoList, defaultIndex } = obj
+  return new Promise((reslove) => {
+    initInstanceVideoPreview(videoList, defaultIndex)
+    videoPreviewInstance.callback = (res) => {
+      reslove(res)
+      // 服务取消时卸载 DOM
+      if (res === 'cancel' && videoPreviewInstance !== null) {
+        document.body.removeChild(videoPreviewInstance.$el)
+        videoPreviewInstance = null
+      }
+    }
+    document.body.appendChild(videoPreviewInstance.$el) //  挂载 DOM
+    Vue.nextTick(() => {
+      videoPreviewInstance.visible = true //  打开视频预览遮罩层
+    })
+  })
 }
 
 export default showVideoPreviewBox
